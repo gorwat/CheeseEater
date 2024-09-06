72d925b27f5919fa5bc7544bf5cd6ca739366d50
Sickan <jessicagorwat@gmail.com>
2024-09-06 20:22:47 +0200

    Init project

---
+++ src/scripts/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+
+@export var speed = 5.0
+@export var jump_velocity = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+# Updates when moving the character
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta):
+
+	# Handle jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		target_velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+		
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		# Setting the basis property will affect the rotation of the node.
+		$Pivot.basis = Basis.looking_at(direction)
+		
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	# Vertical Velocity
+	if not is_on_floor(): 
+		target_velocity.y = target_velocity.y - (gravity * delta)
+	
+	velocity = target_velocity
+	move_and_slide()
