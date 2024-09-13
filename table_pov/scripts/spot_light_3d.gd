fd2f54841e75026bcede0e94e5a912f1dcfeab05
jgorwat <jgorwat@kth.se>
2024-09-13 18:38:01 +0200

    Fix missing dependency for spotlight script. Fix temporary spotlight controls to correspond to current camera

--- table_pov/scripts/spot_light_3d.gd
+++ table_pov/scripts/spot_light_3d.gd
@@ -23,6 +23,7 @@ func _ready():
 		thread = Thread.new()
 		thread.start(_thread_func)
 		get_window().close_requested.connect(clean_func)
+		
 	else:
 		print("Connection failed; execute_with_pipeline returned: ", dict)
 		
@@ -38,19 +39,14 @@ func clean_func():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#var mouse_position = get_viewport().get_mouse_position()
-	#if (self.position.x != mouse_position.x) || (self.position.z != mouse_position.y):
-		#self.position = Vector3(mouse_position.x, self.position.y, mouse_position.y)
-		#spot_position_changed.emit(self.position)	
-	
 	if Input.is_key_pressed(KEY_W):
-		self.position.x += 0.1
-	if Input.is_key_pressed(KEY_S):
-		self.position.x -= 0.1
-	if Input.is_key_pressed(KEY_A):
-		self.position.z -= 0.1
-	if Input.is_key_pressed(KEY_D):
 		self.position.z += 0.1
+	if Input.is_key_pressed(KEY_S):
+		self.position.z -= 0.1
+	if Input.is_key_pressed(KEY_A):
+		self.position.x += 0.1
+	if Input.is_key_pressed(KEY_D):
+		self.position.x -= 0.1
 	
 	spot_position_changed.emit(self.position)
 
