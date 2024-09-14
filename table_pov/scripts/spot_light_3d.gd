90376432267c7e8c87608503b36b81d32bf73abd
Anton Lilja <liljaanton2001@gmail.com>
2024-09-14 16:29:14 +0200

    Fix OS.execute_with_pipe usage
    The problem seems to be with the get_as_text method which seems to want
    to read all the content at once which doesn't work for stdout.
    We can instead use get_line, get_int, get_float etc, i.e functions which
    try to read a limited amount of data.
    
    This commit also restructures the code to launch the process in a
    seperate thread and doesn't launch the process through cmd.exe on
    Windows.

--- table_pov/scripts/spot_light_3d.gd
+++ table_pov/scripts/spot_light_3d.gd
@@ -2,46 +2,31 @@ extends SpotLight3D
 
 signal spot_position_changed
 
-var stdio_pipe
 var thread
+var should_close: bool = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	# assign and check file path
-	var file_path = "res://table_communication/dummy_coordinates" 
-	if FileAccess.file_exists(file_path):
-		print("File exists at path: ", file_path)
-	else:
-		print("File does not exist at path: ", file_path)
-		
 	# setup connection to executable
-	var dict;
-	if OS.get_name() == "Windows":
-		dict = OS.execute_with_pipe("cmd.exe", ["/c", file_path])
-	else:
-		dict = OS.execute_with_pipe(file_path, [])
-	
-	if !dict.is_empty():
-		stdio_pipe = dict["stdio"]
-		
-		thread = Thread.new()
-		thread.start(_thread_func)
-		get_window().close_requested.connect(clean_func)
-		
-	else:
-		print("Connection failed; execute_with_pipeline returned: ", dict)
-		
+	thread = Thread.new()
+	thread.start(_thread_func)
+	get_window().close_requested.connect(clean_func)
+
 func _thread_func():
-	print ("Is open: ", stdio_pipe.is_open())
-	while stdio_pipe.is_open() and stdio_pipe.get_error() == OK:
-		print("Msg: ", stdio_pipe.get_as_text())
+	var dict = OS.execute_with_pipe("table.exe", [])
+	assert(!dict.is_empty())
+	var stdio_pipe = dict["stdio"]
+	while stdio_pipe.is_open() and stdio_pipe.get_error() == OK and !should_close:
+		var x = stdio_pipe.get_float()
+		var y = stdio_pipe.get_float()
+		var z = stdio_pipe.get_float()
+		print(x, y, z)
 	print("Error: ", stdio_pipe.get_error())
+	stdio_pipe.close()
 
 func clean_func():
-	stdio_pipe.close()
+	should_close = true
 	thread.wait_to_finish()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_key_pressed(KEY_W):
 		self.position.z += 0.1
@@ -53,6 +38,3 @@ func _process(delta):
 		self.position.x -= 0.1
 	
 	spot_position_changed.emit(self.position)
-
-	
-	
