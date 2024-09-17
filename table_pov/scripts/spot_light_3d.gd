1dc7c29dfb06208e34d905f1bea78efc717591ca
Anton Lilja <liljaanton2001@gmail.com>
2024-09-17 10:13:58 +0200

    Send native window handle to table child process

--- table_pov/scripts/spot_light_3d.gd
+++ table_pov/scripts/spot_light_3d.gd
@@ -12,7 +12,8 @@ func _ready():
 	get_window().close_requested.connect(clean_func)
 
 func _thread_func():
-	var dict = OS.execute_with_pipe("./table.exe", [])
+	var hwnd = DisplayServer.window_get_native_handle(DisplayServer.WINDOW_HANDLE)
+	var dict = OS.execute_with_pipe("./table.exe", [str(hwnd)])
 	assert(!dict.is_empty())
 	var stdio_pipe = dict["stdio"]
 	var pid = dict["pid"]
--- table_pov/table_communication/dummy_coordinates.c
+++ table_pov/table_communication/dummy_coordinates.c
@@ -1,11 +1,24 @@
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 
-int main(void)
+#include <windows.h>
+
+int main(int argc, char** argv)
 {
+   if(argc != 2) return 1;
+
+#ifdef _WIN64
+   fprintf(stderr, "%s", argv[1]);
+   HWND hwnd = (HWND)strtoull(argv[1], NULL, 10);
+
+   SetWindowPos(hwnd, NULL, 0, 0, 960, 540, 0);
+#endif
+
    float arr[12] = 
-   { 
+   {
       1.0, 1.0,
       0.75, 0.75, 
       0.25, 0.25,
@@ -19,7 +32,6 @@ int main(void)
       if (is_on){
          write(STDOUT_FILENO, &arr[index * 2 + 0], 4);
          write(STDOUT_FILENO, &arr[index * 2 + 1], 4);
-
       }
       index += 1;
       index %= 4;
