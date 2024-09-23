ade8e4e1a10a17edd5149c69d36ccff9e5148425
Isak Larsson <isak.valter.larsson@gmail.com>
2024-09-23 12:44:00 +0200

    Add audio, footseps, ambience, and spotlight hum

--- rat_pov/scripts/spot_light_3d.gd
+++ rat_pov/scripts/spot_light_3d.gd
@@ -1,10 +1,10 @@
 extends SpotLight3D
 
-
+@onready var humming_sound: AudioStreamPlayer3D = $SpotlightHumming
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
+ 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -14,5 +14,7 @@ func _process(delta):
 func _on_network_manager_spot_position_changed(is_on: bool, position : Vector3):
 	self.position = position
 	self.visible = is_on
+	humming_sound.playing = is_on
+	
 	#print("Visible: ", is_on)
 	#print(position)
