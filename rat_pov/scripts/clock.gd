980d7f0a4cd6cb38542a78a5c58c9cdcd6aacf51
Sickan <jessicagorwat@gmail.com>
2024-10-23 01:28:55 +0200

    Fix forcequit from anywhere including timer reset to default

--- rat_pov/scripts/clock.gd
+++ rat_pov/scripts/clock.gd
@@ -1,11 +1,11 @@
 extends Label
 
 @onready var game_timer: Timer = $Timer
-const DEFAULT_TIME = 1*10 
+@export var default_time = 60
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	game_timer.wait_time = DEFAULT_TIME
+	game_timer.wait_time = default_time
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
