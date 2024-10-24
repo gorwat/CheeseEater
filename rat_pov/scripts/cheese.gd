08bde0e8c1c6284e344609be3a66ca6f6c557801
Sickan <jessicagorwat@gmail.com>
2024-10-24 11:48:15 +0200

    Rotating cheese

--- rat_pov/scripts/cheese.gd
+++ rat_pov/scripts/cheese.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 signal been_eaten
 
+#@onready var player: CharacterBody3D = get_node("Player")
+@export var rotate_speed = 0.2
 var is_spawned = true;
 
 func spawn():
@@ -20,12 +22,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-	
+	rotation_degrees.y += rotate_speed
 
 func _on_area_3d_body_entered(body):
 	if not is_spawned:
 		return
+	#if (body == player):
 	despawn()
 	been_eaten.emit(name)
 	get_parent().current_cheese_count -= 1
