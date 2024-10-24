29b149f854a568bd0dbbf7185d696d903c67813c
antlilja <liljaanton2001@gmail.com>
2024-10-24 21:52:18 +0200

    Make bushes move on table

--- table_pov/scripts/player.gd
+++ table_pov/scripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+signal rat_moved(position: Vector3, rotation: Vector3)
+
 func _physics_process(delta):
 	pass
 
@@ -7,3 +9,4 @@ func _physics_process(delta):
 func _on_network_manager_rat_pos_recieved(position : Vector3, rotation : Vector3):
 	self.position = position
 	self.rotation = rotation
+	rat_moved.emit(self.position, self.rotation)
